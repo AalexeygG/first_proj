87199ec7874f5511163366d9579b681aaee91ed2
krakchick <aalexeygg@gmail.com>
2026-04-05 21:50:54 +0300

    Trying env file

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
-print('Hello from repository! йоу')
+# Импорт load_dotenv.
+from dotenv import load_dotenv
 
+# Импорт библиотеки для работы с окружением.
+import os
 
+def print_author():
+    # Загрузка переменных из .env
+    load_dotenv('secret.env')  # здесь путь в скобках, если та же директория, то можно пустым оставить
 
+    # Теперь переменные доступны через os.environ
+    author = os.getenv('AUTHOR')
+    print(f"Автор проекта: {author}")
 
+def main():
+    print_author()
 
+main()
